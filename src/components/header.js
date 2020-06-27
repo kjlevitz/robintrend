6b34217d35c7e7b46b5331c2c52ebfa08ea7fed6
tlhuynh <tuan.huynh@temple.edu>
2020-06-27 14:06:42 -0400

    Small clean up

--- src/components/header.js
+++ src/components/header.js
@@ -9,22 +9,7 @@ export default function Header() {
     return (
         <div className="container">
           <Head>
-            <div className={styles.topnav}>
-                <a href="index" alt="home" src="../media/images/home.png" width="250" height="50">Home</a>
-                <a href="trends">Trends</a>
-                <a href="about">About</a>
-                <a><input type="search" id="stockSymbol" placeholder="Search Stock"></input></a>
-                <a><button type="button" onClick="searchStock()">Search</button></a>
-
-
-            </div>
-
-              <script>
-                  function searchStock() {
-                  console.log("Input: ", document.getElementById("stockSymbol").valueOf())
-              }
-              </script>
-            <Nav />
+              <Nav />
           </Head>
     </div>
 
