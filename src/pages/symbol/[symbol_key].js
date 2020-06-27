e223d6463eb3240902b1f97750b9a22701a30ed2
Kevin Levitz <kevin@klevitz.com>
2020-06-27 13:58:01 -0400

    fix: Persistant Navbar routing fixed when on Symbol page

--- src/pages/symbol/[symbol_key].js
+++ src/pages/symbol/[symbol_key].js
@@ -1,8 +1,23 @@
 import {useRouter} from 'next/router';
+import Header from '../../components/Header.js'
+import Footer from '../../components/Footer.js'
 
 ///symbol/GOOG
 export default function Symbol_Key(){
     const router = useRouter();
     //console.log(router.query);
-    return <h1>Symbol is {router.query.symbol_key}</h1>
+    return(
+        <div className="container">
+            <Header />
+            <main>
+                <h1>Symbol is {router.query.symbol_key}</h1>
+
+            </main>
+            
+            <Footer />
+        </div>
+
+
+
+    ); 
 }
